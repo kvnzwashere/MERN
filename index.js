975b5a966ae73673e88f694b904455b22c3ea153
kvnzwashere <87238723+kvnzwashere@users.noreply.github.com>
2023-12-15 02:18:47 +0700

    Add files via upload
    Regist main.js

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 import React from 'react'
-import { LoginBg } from '../../assets';
+import './register.scss';
+import { RegisterBg } from '../../assets';
 import { Button, Gap, Input, Link } from '../../assets/components';
 
-const Login = () => {
+const Register = () => {
   return (
     <div className="main-page">
       <div className="left">
-        <img src={LoginBg} className="bg-image" alt="imageBg" />
+        <img src={RegisterBg} className="bg-image" alt="imageBg" />
     </div>
     <div className="right">
-        <p className="title">Login</p>
-      
+        <p className="title">Register</p>
+        <Input label="Full Name" placeholder="Full Name" />
+        <Gap height={20} />
         <Input label="Email" placeholder="Email" />
         <Gap height={20} />
         <Input label="Password" placeholder="Password" />
         <Gap height={35} />
-        <Button title="Login" />
+        <Button title="Register" />
         <Gap height={100} />
-        <Link title="Belum Punya Akun?" />
+        <Link title="Kembali ke Login" />
+
+      
         </div>
-      </div>
+    </div>
   )
 }
 
-export default Login
+export default Register
